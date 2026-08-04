1bb585bc5d7a816891854e4f2e9378213f6e4d55
Abhinav <abhinavnyps@gmail.com>
2026-08-04 22:12:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ if "indexed_filename" not in st.session_state:
 # --------------------------------------------------------------------------------------
 with st.sidebar:
     api_key=os.environ.get("GOOGLE_API_KEY", "")
+    model_name="gemini-2.5-flash"
     st.header("⚙️ Configuration")
     st.subheader("📄 Upload a PDF")
     uploaded_pdf = st.file_uploader("Choose a PDF file", type=["pdf"])
